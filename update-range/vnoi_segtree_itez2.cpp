10f2044ab6af8a92bd1ad91e7d57ffe9275ee91a
Tuan <mtuann4@gmail.com>
2024-06-16 10:43:52 +0700

    update seg itez2

--- update-range/vnoi_segtree_itez2.cpp
+++ update-range/vnoi_segtree_itez2.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 using namespace std;
 const int INF = 1e9;
 const int MAXN = 1e5 + 10;
 int n;
 
-int tree[4 * MAXN];
+long long tree[4 * MAXN];
 
 
 void updatePoint(int start, int end, int idx, int val, int node) {
     if (start == end) {
-        arr[idx] = val;
         tree[node] = val;
         return;
     }
@@ -20,22 +20,22 @@ void updatePoint(int start, int end, int idx, int val, int node) {
     } else {
         updatePoint(mid + 1, end, idx, val, 2 * node + 1);
     }
-    tree[node] = max(tree[2 * node], tree[2 * node + 1]);
+    tree[node] = tree[2 * node] + tree[2 * node + 1];
 }
 
 
-int queryRangeUtil(int start, int end, int l, int r, int node) {
+long long queryRangeUtil(int start, int end, int l, int r, int node) {
     
     if (start > end || start > r || end < l)
-        return -INF;
+        return 0;
 
     if (start >= l && end <= r)
         return tree[node];
 
     int mid = (start + end) / 2;
-    int leftMax = queryRangeUtil(start, mid, l, r, 2 * node);
-    int rightMax = queryRangeUtil(mid + 1, end, l, r, 2 * node + 1);
-    return max(leftMax, rightMax);
+    long long leftSum = queryRangeUtil(start, mid, l, r, 2 * node);
+    long long rightSum = queryRangeUtil(mid + 1, end, l, r, 2 * node + 1);
+    return leftSum + rightSum;
 }
 
 
@@ -67,4 +67,4 @@ int main() {
     return 0;
 }
 
-// g++ -std=c++17 -o a vnoi_segtree_itez1.cpp && ./a < a.in
+// g++ -std=c++17 -o a vnoi_segtree_itez2.cpp && ./a < a.in
