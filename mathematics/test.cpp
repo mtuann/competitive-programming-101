85d7f8b5db394dbd9322c800fe7725db2450fb5e
Tuan <mtuann4@gmail.com>
2024-07-19 23:26:40 +0700

    add string

--- mathematics/test.cpp
+++ mathematics/test.cpp
@@ -1,173 +1,72 @@
-// A C++ program to find convex hull of a set of points. Refer
-// https://www.geeksforgeeks.org/orientation-3-ordered-points/
-// for explanation of orientation()
 #include <iostream>
-#include <stack>
-#include <stdlib.h>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define pii pair<int, int>
+#define fi first
+#define se second
+
 using namespace std;
-
-struct Point
-{
-	int x, y;
-};
-
-// A global point needed for sorting points with reference
-// to the first point Used in compare function of qsort()
-Point p0;
-
-// A utility function to find next to top in a stack
-Point nextToTop(stack<Point> &S)
-{
-	Point p = S.top();
-	S.pop();
-	Point res = S.top();
-	S.push(p);
-	return res;
-}
-
-// A utility function to swap two points
-void swap(Point &p1, Point &p2)
-{
-	Point temp = p1;
-	p1 = p2;
-	p2 = temp;
-}
-
-// A utility function to return square of distance
-// between p1 and p2
-int distSq(Point p1, Point p2)
-{
-	return (p1.x - p2.x)*(p1.x - p2.x) +
-		(p1.y - p2.y)*(p1.y - p2.y);
-}
-
-// To find orientation of ordered triplet (p, q, r).
-// The function returns following values
-// 0 --> p, q and r are collinear
-// 1 --> Clockwise
-// 2 --> Counterclockwise
-int orientation(Point p, Point q, Point r)
-{
-	int val = (q.y - p.y) * (r.x - q.x) -
-			(q.x - p.x) * (r.y - q.y);
-
-	if (val == 0) return 0; // collinear
-	return (val > 0)? 1: 2; // clock or counterclock wise
-}
-
-// A function used by library function qsort() to sort an array of
-// points with respect to the first point
-int compare(const void *vp1, const void *vp2)
-{
-Point *p1 = (Point *)vp1;
-Point *p2 = (Point *)vp2;
-
-// Find orientation
-int o = orientation(p0, *p1, *p2);
-if (o == 0)
-	return (distSq(p0, *p2) >= distSq(p0, *p1))? -1 : 1;
-
-return (o == 2)? -1: 1;
-}
-
-// Prints convex hull of a set of n points.
-void convexHull(Point points[], int n)
-{
-// Find the bottommost point
-int ymin = points[0].y, min = 0;
-for (int i = 1; i < n; i++)
-{
-	int y = points[i].y;
-
-	// Pick the bottom-most or choose the left
-	// most point in case of tie
-	if ((y < ymin) || (ymin == y &&
-		points[i].x < points[min].x))
-		ymin = points[i].y, min = i;
-}
-
-// Place the bottom-most point at first position
-swap(points[0], points[min]);
-
-// Sort n-1 points with respect to the first point.
-// A point p1 comes before p2 in sorted output if p2
-// has larger polar angle (in counterclockwise
-// direction) than p1
-p0 = points[0];
-cout << "Base point: " << p0.x << " " << p0.y << endl;
-qsort(&points[1], n-1, sizeof(Point), compare);
-
-for (int i = 0; i < n; i++) {
-    cout << points[i].x << " " << points[i].y << endl;
-}
-
-// If two or more points make same angle with p0,
-// Remove all but the one that is farthest from p0
-// Remember that, in above sorting, our criteria was
-// to keep the farthest point at the end when more than
-// one points have same angle.
-int m = 1; // Initialize size of modified array
-for (int i=1; i<n; i++)
-{
-	// Keep removing i while angle of i and i+1 is same
-	// with respect to p0
-	while (i < n-1 && orientation(p0, points[i],
-									points[i+1]) == 0)
-		i++;
-
-
-	points[m] = points[i];
-	m++; // Update size of modified array
-}
-
-// If modified array of points has less than 3 points,
-// convex hull is not possible
-if (m < 3) return;
-
-// Create an empty stack and push first three points
-// to it.
-stack<Point> S;
-S.push(points[0]);
-S.push(points[1]);
-S.push(points[2]);
-
-// Process remaining n-3 points
-for (int i = 3; i < m; i++)
-{
-	// Keep removing top while the angle formed by
-	// points next-to-top, top, and points[i] makes
-	// a non-left turn
-	while (S.size()>1 && orientation(nextToTop(S), S.top(), points[i]) != 2)
-		S.pop();
-	S.push(points[i]);
-}
-
-// Now stack has the output points, print contents of stack
-while (!S.empty())
-{
-	Point p = S.top();
-	cout << "(" << p.x << ", " << p.y <<")" << endl;
-	S.pop();
-}
-}
-
-Point points[100000];
-
-// Driver program to test above functions
-int main()
-{
-	// Point points[] = {{0, 3}, {1, 1}, {2, 2}, {4, 4},
-	// 				{0, 0}, {1, 2}, {3, 1}, {3, 3}};
-
-	// int n = sizeof(points)/sizeof(points[0]);
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> points[i].x >> points[i].y;
+const int inf = 1e9 + 7;
+const int MAXN = 1e7 + 7;
+const int MAXK = 1e5 + 7;
+const int MAXSUB2 = 2e2 + 10;
+const int MOD = 1e9 + 7;
+ll dp[MAXSUB2][MAXSUB2];
+int n, k, d, sl;
+int b[MAXN];
+vector<int> a;
+namespace sub12 {
+    bool check() {
+        if (n - k <= 2e2 + 10) return 1;
+        return 0;
     }
-	convexHull(points, n);
-	return 0;
+    void solve() {
+        for (int i = 0; i < sl; i++) dp[i][1] = 1;
+        dp[1][1] = 1;
+        for (int i = 1; i <= sl; i++) {
+            for (int j = 1; j < sl; j++) {
+                for (int k = j - 1; k >= 0; k--) {
+                    if (a[j] - a[k] > d) {
+                        dp[j][i] = (dp[j][i] + dp[k][i-1]) % MOD;
+                    }
+                }
+            }
+        }
+        for (int i = sl; i >= 0; i--) {
+            ll temp = 0;
+            for (int j = 0; j < sl; j++) {
+                temp = (temp + dp[j][i]) % MOD;
+            }
+            if (temp > 0) {
+                cout << i << endl << temp;
+                return;
+            }
+        }
+    }
 }
-
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    memset(dp, 0, sizeof(dp));
+    cin >> n >> k >> d;
+    sl = n-k;
+	// memset(b, 0, sizeof(b));
+    for (int i = 0; i < k; i++) {
+        int x;
+        cin >> x;
+        b[x] = 1;
+		cout << x << endl;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (b[i] == 0) {
+			a.push_back(i);
+			// cout << i << endl;
+		}
+    }
+	cout << sl << " " << int(a.size()) << endl;
+    if (sub12::check() == 1) {sub12::solve(); return 0;}
+    return 0;
+}
 
 // g++ -std=c++14 -o a test.cpp && ./a < a.in
+
---
+++ string-algorithms/hash.cpp
@@ -0,0 +1,119 @@
+// Source template from PVH: https://ideone.com/iEeYmj
+// Problem: https://usaco.org/index.php?page=viewproblem2&cpid=529
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#define ll long long
+
+#define NMOD 2
+const int BASE = 256;
+const int MAXN = (int)1e6 + 10;
+const int MOD[] = {(int)1e9 + 2277, (int)1e9 + 5277, (int)1e9 + 8277, (int)1e9 + 9277};
+
+using namespace std;
+
+int pw[NMOD][MAXN];
+
+struct Hash {
+    int value[NMOD];
+    Hash() {
+        memset(value, 0, sizeof(value));
+    }
+
+    Hash(char c) {
+        for (int i = 0; i < NMOD; i++) {
+            value[i] = c - 'a';
+        }
+    }
+
+    Hash operator + (const Hash &other) const {
+        Hash res;
+        for (int i = 0; i < NMOD; i++) {
+            res.value[i] = (1LL * value[i] * BASE + other.value[i]) % MOD[i];
+        }
+        return res;
+    }
+
+    bool operator == (const Hash &other) const {
+        for (int i = 0; i < NMOD; i++) {
+            if (value[i] != other.value[i]) return false;
+        }
+        return true;
+    }
+
+};
+
+Hash hs[MAXN];
+
+void init(char s[], int n) {
+    for (int i = 0; i < NMOD; i++) {
+        pw[i][0] = 1;
+        for (int j = 1; j < MAXN; j++) {
+            pw[i][j] = 1LL * pw[i][j - 1] * BASE % MOD[i];
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        hs[i] = hs[i - 1] + Hash(s[i - 1]);
+    }
+}
+
+char s[MAXN];
+char t[MAXN];
+char res[MAXN];
+
+Hash get_hash(int l, int r) {
+    Hash res;
+    for (int i = 0; i < NMOD; i++) {
+        res.value[i] = (hs[r].value[i] - (1LL * hs[l - 1].value[i] * pw[i][r - l + 1] % MOD[i])) % MOD[i];
+        if (res.value[i] < 0) res.value[i] += MOD[i];
+    }
+    return res;
+}
+
+void solve(int n, int m, char s[], char t[]) {
+    Hash ht;
+    for (int i = 0; i < m; i++) {
+        ht = ht + Hash(t[i]);
+        // cout << Hash(t[i]).value[0] << " ";
+    }
+
+    // for (int i = 0; i < NMOD; i++) {
+    //     cout << ht.value[i] << " ";
+    // }
+
+    int cur_len = 0;
+    for (int i = 0; i < n; i++) {
+        hs[cur_len + 1] = hs[cur_len] + Hash(s[i]);
+        res[cur_len] = s[i];
+        cur_len++;
+
+        if (cur_len < m) continue;
+        
+        Hash cur_hs = get_hash(cur_len - m + 1, cur_len);
+        // cout << cur_len << " " << cur_hs.value[0] << "\n";
+        if (cur_hs == ht)  cur_len -= m;
+
+    }
+
+    for (int i = 0; i < cur_len; i++) {
+        cout << res[i];
+    }
+
+}
+
+int main() {
+    freopen("censor.in", "r", stdin);
+    freopen("censor.out", "w", stdout);
+
+    scanf("%s", s);
+    int n = strlen(s);
+    init(s, n);
+    scanf("%s", t);
+    int m = strlen(t);
+    solve(n, m, s, t);
+
+    return 0;
+}
+// g++ -std=c++14 -o a hash.cpp && ./a < a.in
